6bdfdff719c9954dd954e2d3e3b2a3dc5cb2d75c
LibertarianFlame <rusandya@gmail.com>
2018-10-15 13:35:01 +0300

    Initial commit

---
+++ Selenium2/Program.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium2
+{
+    class Program
+    {
+        static IWebDriver driver;
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Init();
+
+                Test1();
+
+                //CleanUp();
+
+
+            }
+            catch (WebDriverException e)
+            {
+
+            }
+        }
+
+        static void Init()
+        {
+            // Создаём ссылку на браузер
+            driver = new ChromeDriver();
+        }
+
+        static void Test1()
+        {
+            // Переходим на тестируемую страницу
+            driver.Navigate().GoToUrl("https://smartbear.com/");
+            // PhraseToSearch
+            //SeleniumLib.EnterText(driver, "PhraseToSearch", "TestComplete", "Id");
+            //SeleniumLib.EnterText(driver, "PhraseToSearch", Keys.Enter, "Id");
+
+            // //*[@id="main-topnav"]/ul/li[1]/a
+            driver.FindElement(By.XPath("//*[@id=\"main-topnav\"]/ul/li[1]/a")).Click();
+        }
+
+        static void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ Selenium2/SeleniumLib.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium2
+{
+    class SeleniumLib
+    {
+        //EnterText
+        public static void EnterText(IWebDriver driver, string element, string value, string elementype)
+        {
+            if (elementype == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            }
+            if (elementype == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value);
+            }
+        }
+
+        // Click
+        public static void Click(IWebDriver driver, string element, string elementype)
+        {
+            if (elementype == "Id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            if (elementype == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click();
+            }
+        }
+
+        // Selecting DropDown Control
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementype)
+        {
+            //SelectDropDown
+            if (elementype == "Id")
+            {
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            }
+            if (elementype == "Name")
+            {
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+            }
+        }
+    }
+}
