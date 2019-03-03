7897393ff04e23d1af9dddbeac6ecf0efa2329fc
LibertarianFlame <rusandya@gmail.com>
2019-03-03 21:16:13 +0300

    Commit3
    This reverts commit 94d49e90b42dc3ff467311d048756a7e859bfd3d.
    
    + add 2 wlriteln

--- Selenium2/Program.cs
+++ Selenium2/Program.cs
@@ -5,19 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace Selenium2
 {
-    [TestFixture]
     class Program
     {
         static IWebDriver driver;
         static void Main(string[] args)
         {
+
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
             /*try
+
+
+            try
+
             {
                 Init();
 
@@ -33,58 +39,25 @@ namespace Selenium2
             }*/
         }
 
-        [SetUp]
-        static public void Init()
+        static void Init()
         {
-            // Создаём ссылку на браузер 
+            // Создаём ссылку на браузер
             driver = new ChromeDriver();
         }
 
-        [Test]
-        static public void Test1()
+        static void Test1()
         {
             // Переходим на тестируемую страницу
-            driver.Navigate().GoToUrl("https://google.com/");
-            SeleniumLib.SetText(driver, "q", "Mandelbrot", PropertyType.Id);
+            driver.Navigate().GoToUrl("https://smartbear.com/");
+            // PhraseToSearch
+            //SeleniumLib.EnterText(driver, "PhraseToSearch", "TestComplete", "Id");
+            //SeleniumLib.EnterText(driver, "PhraseToSearch", Keys.Enter, "Id");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // кликаем по Products
+            // //*[@id="main-topnav"]/ul/li[1]/a
             driver.FindElement(By.XPath("//*[@id=\"main-topnav\"]/ul/li[1]/a")).Click();
-            // кликаем по TestComplete
-            SeleniumLib.Click(driver, "TestComplete", PropertyType.LinkText);
-            // кликаем по FreeTrial
-            SeleniumLib.Click(driver, "product_navigation__cta", PropertyType.ClassName);
-            // вводим email 
-            SeleniumLib.SetText(driver, "Email", "example@gmail.com", PropertyType.Id);
-            // проверяем, что в окне правильное знаечение
-            string t = SeleniumLib.GetText(driver, "Email", "Id");
-            Console.WriteLine(t);
-            Assert.AreEqual("example@gmail.com", t);
-            // выбираем в списке стран "Russian Federation"
-            SeleniumLib.SelectDropDown(driver, "Country", "Russian Federation", "Id");
-            // проверяем, что выбралась Россия
-            string s = SeleniumLib.GetTextFromDropdownList(driver, "Country", "Id");
-            Console.WriteLine(s);
-
-            //string s = SeleniumLib.GetText(driver, "product-cta-buttons", "ClassName");
-            //Console.WriteLine(s);
         }
-        [TearDown]
-        static public void CleanUp()
+
+        static void CleanUp()
         {
             driver.Close();
         }
--- Selenium2/SeleniumLib.cs
+++ Selenium2/SeleniumLib.cs
@@ -9,79 +9,38 @@ using OpenQA.Selenium.Support.UI;
 
 namespace Selenium2
 {
-    enum PropertyType
-    {
-        Id,
-        Name,
-        ClassName,
-        LinkText,
-        CssName,
-        XPath
-    }
     class SeleniumLib
     {
-
         //EnterText
-        public static void SetText(IWebDriver driver, string element, string value, PropertyType elementype)
+        public static void EnterText(IWebDriver driver, string element, string value, string elementype)
         {
-            if (elementype == PropertyType.Id)
+            if (elementype == "Id")
             {
                 driver.FindElement(By.Id(element)).SendKeys(value);
             }
-            if (elementype == PropertyType.Name)
+            if (elementype == "Name")
             {
                 driver.FindElement(By.Name(element)).SendKeys(value);
-            }
-            if (elementype == PropertyType.ClassName)
-            {
-                driver.FindElement(By.ClassName(element)).SendKeys(value);
-            }
-            if (elementype == PropertyType.LinkText)
-            {
-                driver.FindElement(By.LinkText(element)).SendKeys(value);
-            }
-            if (elementype == PropertyType.CssName)
-            {
-                driver.FindElement(By.CssSelector(element)).SendKeys(value);
-            }
-            if (elementype == PropertyType.XPath)
-            {
-                driver.FindElement(By.XPath(element)).SendKeys(value);
             }
         }
 
         // Click
-        public static void Click(IWebDriver driver, string element, PropertyType elementype)
+        public static void Click(IWebDriver driver, string element, string elementype)
         {
-            if (elementype == PropertyType.Id)
+            if (elementype == "Id")
             {
                 driver.FindElement(By.Id(element)).Click();
             }
-            if (elementype == PropertyType.Name)
+            if (elementype == "Name")
             {
                 driver.FindElement(By.Name(element)).Click();
-            }
-            if (elementype == PropertyType.ClassName)
-            {
-                driver.FindElement(By.ClassName(element)).Click();
-            }
-            if (elementype == PropertyType.LinkText)
-            {
-                driver.FindElement(By.LinkText(element)).Click();
-            }
-            if (elementype == PropertyType.CssName)
-            {
-                driver.FindElement(By.CssSelector(element)).Click();
-            }
-            if (elementype == PropertyType.XPath)
-            {
-                driver.FindElement(By.XPath(element)).Click();
             }
         }
 
         // Selecting DropDown Control
         public static void SelectDropDown(IWebDriver driver, string element, string value, string elementype)
         {
+            //SelectDropDown
             if (elementype == "Id")
             {
                 new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
@@ -90,50 +49,6 @@ namespace Selenium2
             {
                 new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
             }
-            if (elementype == "ClassName")
-            {
-                new SelectElement(driver.FindElement(By.ClassName(element))).SelectByValue(value);
-            }
-            if (elementype == "LinkText")
-            {
-                new SelectElement(driver.FindElement(By.LinkText(element))).SelectByValue(value);
-            }
-        }
-
-        public static string GetText(IWebDriver driver, string element, string elementype)
-        {
-            if (elementype == "Id")
-            {
-                return driver.FindElement(By.Id(element)).GetAttribute("value");
-            }
-            if (elementype == "Name")
-            {
-                return driver.FindElement(By.Name(element)).GetAttribute("value");
-            }
-            if (elementype == "ClassName")
-            {
-                return driver.FindElement(By.ClassName(element)).GetAttribute("value");
-            }
-
-            return String.Empty;
-        }
-
-        public static string GetTextFromDropdownList(IWebDriver driver, string element, string elementype)
-        {
-            if (elementype == "Id")
-            {
-                return new SelectElement(driver.FindElement(By.Id(element))).AllSelectedOptions.SingleOrDefault().Text;
-            }
-            if (elementype == "Name")
-            {
-                return new SelectElement(driver.FindElement(By.Name(element))).AllSelectedOptions.SingleOrDefault().Text;
-            }
-            if (elementype == "ClassName")
-            {
-                return new SelectElement(driver.FindElement(By.ClassName(element))).AllSelectedOptions.SingleOrDefault().Text;
-            }
-
-            return String.Empty;
         }
     }
 }
